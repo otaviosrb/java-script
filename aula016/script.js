3f5f2f6c4692ebcc49abe96e5bc0a57c3e6aefe5
otaviosrb <150717463+otaviosrb@users.noreply.github.com>
2023-11-20 22:16:51 -0300

    add aula 016 até 019

--- aula016/script.js
+++ aula016/script.js
@@ -1,9 +1,11 @@
+const objs = document.getElementsByTagName('div')
+
 let num = [10, 20, 30, 40, 50]
 
-for(n in num){
-    console.log(num[n])
+for(o of objs){
+    console.log(o)
 }
 
-// for(let i=0; i < num.length; i++) {
-//     console.log(num[i])
-// }
+for(o in objs){
+    console.log(o)
+}
