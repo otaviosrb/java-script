b487acffcac47a444d2236ae594207debddd6504
otaviosrb <150717463+otaviosrb@users.noreply.github.com>
2023-11-29 20:37:25 -0300

    add aula 26-27-28

---
+++ aula028/script.js
@@ -0,0 +1,21 @@
+const el = document.getElementsByTagName('div')
+el.map((e, i) => {
+    console.log(e)
+})
+console.log(el)
+
+
+
+
+
+
+
+
+
+
+// const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
+// let c = cursos.map((el, i) => {
+//     return el
+// })
+
+// console.log(c)
