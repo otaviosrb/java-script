1a51a35bcf87a00ce7d637fd76e6375b9492375a
otaviosrb <150717463+otaviosrb@users.noreply.github.com>
2023-12-06 10:57:06 -0300

    finalizei aula028

--- aula028/script.js
+++ aula028/script.js
@@ -1,16 +1,20 @@
-const el = document.getElementsByTagName('div')
-el.map((e, i) => {
-    console.log(e)
-})
-console.log(el)
+const converterInt = (e) => parseInt(e)
+const dobrar = (e) => e*2
+let num = ['1' ,'2', '3', '4', '5'].map(converterInt)
+console.log(num)
+
+// const el = document.getElementsByTagName('div')
+// const val = Array.prototype.map.call(el,({innerHTML})=>innerHTML)
+
+// console.log(val)
 
 
-
-
-
-
-
-
+// let el = document.getElementsByTagName('div')
+// el = [...el]
+// console.log(el)
+// el.map((e, i) => {
+//     e.innerHTML = 'Curso JS'
+// })
 
 
 // const cursos = ['HTML', 'CSS', 'JavaScript', 'PHP', 'React']
